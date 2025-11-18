277907db1a7277b5bfc091a0d860486478f88118
drumath2237 <2237k.drumath@gmail.com>
2025-11-19 02:46:02 +0900

    :sparkles: setup threejs scene

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,53 @@
 import "./style.css";
 
+import {
+  BoxGeometry,
+  DirectionalLight,
+  Mesh,
+  MeshStandardMaterial,
+  PerspectiveCamera,
+  Scene,
+  WebGLRenderer,
+} from "three";
+
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+
 function main() {
-  console.log("hello!");
+  const renderCanvas = document.querySelector<HTMLCanvasElement>(
+    "#renderCanvas",
+  );
+  if (!renderCanvas) {
+    return;
+  }
+
+  const scene = new Scene();
+  const camera = new PerspectiveCamera(
+    60,
+    window.innerWidth / window.innerHeight,
+    0.01,
+    20,
+  );
+  camera.position.set(0, 0, -1);
+  const renderer = new WebGLRenderer({ antialias: true, canvas: renderCanvas });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  const geometry = new BoxGeometry(0.2, 0.2, 0.2);
+  const material = new MeshStandardMaterial({ color: 0xffffff });
+  const mesh = new Mesh(geometry, material);
+  scene.add(mesh);
+
+  const light = new DirectionalLight();
+  light.position.set(1, 2, -1);
+  light.target = mesh;
+  scene.add(light);
+
+  const controls = new OrbitControls(camera, renderCanvas);
+
+  renderer.setAnimationLoop((time) => {
+    renderer.render(scene, camera);
+    controls.update(time);
+    mesh.rotateY(0.01);
+  });
 }
 
 main();
