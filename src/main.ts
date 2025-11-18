1c7f047469fbded90e4fdd6b15e638c023f100a7
drumath2237 <2237k.drumath@gmail.com>
2025-11-19 03:39:07 +0900

    :bento: add sushi.sog

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,17 @@
 import "./style.css";
 
-import { SplatMesh } from "@sparkjsdev/spark";
+import { SplatLoader, SplatMesh } from "@sparkjsdev/spark";
 import { PerspectiveCamera, Scene, WebGLRenderer } from "three";
 
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
-import gsAssetPath from "../assets/pizza.sog?url";
+import sushi from "../assets/sushi.sog?url";
+import pizza from "../assets/pizza.sog?url";
 
-function main() {
-  const renderCanvas =
-    document.querySelector<HTMLCanvasElement>("#renderCanvas");
+async function main() {
+  const renderCanvas = document.querySelector<HTMLCanvasElement>(
+    "#renderCanvas",
+  );
   if (!renderCanvas) {
     return;
   }
@@ -25,11 +27,22 @@ function main() {
   const renderer = new WebGLRenderer({ antialias: true, canvas: renderCanvas });
   renderer.setSize(window.innerWidth, window.innerHeight);
 
-  const splat = new SplatMesh({ url: gsAssetPath });
-  splat.rotateX(Math.PI);
-  scene.add(splat);
-
   const controls = new OrbitControls(camera, renderCanvas);
+
+  const sushiSplat = new SplatMesh({ url: sushi });
+  sushiSplat.rotateX(Math.PI);
+  sushiSplat.position.set(0.1, 0.02, -0.1);
+  scene.add(sushiSplat);
+
+  const sushiSplat2 = new SplatMesh({ packedSplats: sushiSplat.packedSplats });
+  sushiSplat2.rotateX(Math.PI);
+  sushiSplat2.position.set(-0.3, 0.02, -0.1);
+  scene.add(sushiSplat2);
+
+  const pizzaSplat = new SplatMesh({ url: pizza });
+  pizzaSplat.rotateX(Math.PI);
+  pizzaSplat.position.set(-0.1, 0, 0);
+  scene.add(pizzaSplat);
 
   renderer.setAnimationLoop((time) => {
     renderer.render(scene, camera);
