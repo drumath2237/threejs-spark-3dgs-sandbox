0d69090883c86a52f7418d72502a87af7825b945
drumath2237 <2237k.drumath@gmail.com>
2025-11-19 02:52:05 +0900

    :sparkles: show splat data

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,15 @@
 import "./style.css";
 
-import {
-  BoxGeometry,
-  DirectionalLight,
-  Mesh,
-  MeshStandardMaterial,
-  PerspectiveCamera,
-  Scene,
-  WebGLRenderer,
-} from "three";
+import { SplatMesh } from "@sparkjsdev/spark";
+import { PerspectiveCamera, Scene, WebGLRenderer } from "three";
 
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 import gsAssetPath from "../assets/pizza.sog?url";
 
 function main() {
-  const renderCanvas = document.querySelector<HTMLCanvasElement>(
-    "#renderCanvas",
-  );
+  const renderCanvas =
+    document.querySelector<HTMLCanvasElement>("#renderCanvas");
   if (!renderCanvas) {
     return;
   }
@@ -29,26 +21,19 @@ function main() {
     0.01,
     20,
   );
-  camera.position.set(0, 0, -1);
+  camera.position.set(0, 0.2, -0.4);
   const renderer = new WebGLRenderer({ antialias: true, canvas: renderCanvas });
   renderer.setSize(window.innerWidth, window.innerHeight);
 
-  const geometry = new BoxGeometry(0.2, 0.2, 0.2);
-  const material = new MeshStandardMaterial({ color: 0xffffff });
-  const mesh = new Mesh(geometry, material);
-  scene.add(mesh);
-
-  const light = new DirectionalLight();
-  light.position.set(1, 2, -1);
-  light.target = mesh;
-  scene.add(light);
+  const splat = new SplatMesh({ url: gsAssetPath });
+  splat.rotateX(Math.PI);
+  scene.add(splat);
 
   const controls = new OrbitControls(camera, renderCanvas);
 
   renderer.setAnimationLoop((time) => {
     renderer.render(scene, camera);
     controls.update(time);
-    mesh.rotateY(0.01);
   });
 }
 
